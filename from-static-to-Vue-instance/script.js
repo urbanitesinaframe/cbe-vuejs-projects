ecdc793a80352c054cbade8e4a26fa8e72b0ac48
Jimmy Ng <jimmy_ng@outlook.de>
2023-06-23 15:12:30 +0200

    refact: using computed

--- from-static-to-Vue-instance/script.js
+++ from-static-to-Vue-instance/script.js
@@ -2,6 +2,12 @@ Vue.createApp({
   data() {
     return {
       name: "Jimmy Ng",
+      regionCode: "en-en",
     };
+  },
+  computed: {
+    currentDate() {
+      return new Date().toLocaleString(this.regionCode);
+    },
   },
 }).mount("#app");
